7114835a362d6c8d14b6fb991a1d448e4afacf5a
Chance B. Loveday <141525952+cloveday098@users.noreply.github.com>
2024-10-05 12:59:35 -0400

    monthlyPayment unit tests #1-5; Testing for 0-value cases

--- Team_Red/src/team_red/Helper.java
+++ Team_Red/src/team_red/Helper.java
@@ -12,28 +12,30 @@
 // monthlyPayment funct
 // Unit tests for both
 public class Helper {
-    public static int add(int a, int b) {
-        return a+b;
-    }
     public static boolean isPos(double n) {
         return n>0;
     }
 
     public static double monthlyPayment(double amt, double annInterestRate, int yrs) {
-        
+        String msg = "";
         if (!isPos(yrs)) {
-            System.out.println("Nice try. We'll look at 1 year instead.");
-            yrs = 1;
+            msg += "Years must be > 0!\n";
         }
         if (!isPos(annInterestRate)) {
             System.out.println("Nice try.");
-            annInterestRate = 1;
+            msg += "Annual interest rate must be > 0!\n";
         }
-        // There shouldn't be a need to validate amt > 0
+
+        if (!msg.equals("")) {
+            System.out.println(msg);
+            return -1;
+        }
         
-        double exp = Math.pow((1+annInterestRate/12), (yrs * 12));
+        // Check for values more precise than pennies ( < 0.01)
+
+        // There shouldn't be a need to validate amt > 0
+        double exp = Math.pow((1+annInterestRate/1200), (yrs * 12));
         System.out.println((amt * annInterestRate/1200) / (1 - 1 / (exp)));
         return (amt * annInterestRate/1200) / (1 - 1 / (exp));
-        
     }
 }
--- Team_Red/test/HelperTest.java
+++ Team_Red/test/HelperTest.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,38 +17,6 @@ public class HelperTest {
     public HelperTest() {
         
  }
-
-    /**
-     * Tests of add method, of class Helper.
-     */
-    @Test
-    public void testAdd1() {
-        System.out.println("add#1");
-        int a = 0;
-        int b = 0;
-        int expResult = 0;
-        int result = Helper.add(a, b);
-        assertEquals(expResult, result);
-    }
-    @Test
-    public void testAdd2() {
-        System.out.println("add#2");
-        int a = 999999999;
-        int b = 999999999;
-        int expResult = 1999999998;
-        int result = Helper.add(a, b);
-        assertEquals(expResult, result);
-    }
-    @Test
-    public void testAdd3() {
-        System.out.println("add#3");
-        int a = -1;
-        int b = -1;
-        int expResult = -2;
-        int result = Helper.add(a, b);
-        assertEquals(expResult, result);
-    }
-
     /**
      * Test of isPos method, of class Helper.
      */
@@ -63,24 +33,61 @@ public class HelperTest {
     /**
      * Test of monthlyPayment method, of class Helper.
      */
+    // 1) All 0's
     @Test
     public void testMonthlyPayment1() {
-        System.out.println("monthlyPayment");
+        System.out.println("monthlyPayment1");
         double amt = 0.0;
         double annInterestRate = 0.0;
         int yrs = 0;
+        double expResult = -1.0;
+        double result = Helper.monthlyPayment(amt, annInterestRate, yrs);
+        assertEquals(expResult, result, 0);
+    }
+    
+    // amt == 0
+    @Test
+    public void testMonthlyPayment2() {
+        System.out.println("monthlyPayment2");
+        double amt = 0.0;
+        double annInterestRate = 5.0;
+        int yrs = 5;
         double expResult = 0.0;
         double result = Helper.monthlyPayment(amt, annInterestRate, yrs);
         assertEquals(expResult, result, 0);
     }
     
+    // annIterestRate == 0
     @Test
-    public void testMonthlyPayment2() {
-        System.out.println("monthlyPayment");
+    public void testMonthlyPayment3() {
+        System.out.println("monthlyPayment3");
+        double amt = 1000.0;
+        double annInterestRate = 0.0;
+        int yrs = 5;
+        double expResult = -1.0;
+        double result = Helper.monthlyPayment(amt, annInterestRate, yrs);
+        assertEquals(expResult, result, 0);
+    }
+    
+    // yrs == 0
+    @Test
+    public void testMonthlyPayment4() {
+        System.out.println("monthlyPayment3");
+        double amt = 1000.0;
+        double annInterestRate = 5.0;
+        int yrs = 0;
+        double expResult = -1.0;
+        double result = Helper.monthlyPayment(amt, annInterestRate, yrs);
+        assertEquals(expResult, result, 0);
+    }
+    
+    @Test
+    public void testMonthlyPayment5() {
+        System.out.println("monthlyPayment5");
         double amt = 10000;
         double annInterestRate = 15.0;
         int yrs = 5;
-        double expResult = 125.0;
+        double expResult = 125/(1 - 1 / (Math.pow((1+15.0/1200), (60))));
         double result = Helper.monthlyPayment(amt, annInterestRate, yrs);
         assertEquals(expResult, result, 0);
     }
