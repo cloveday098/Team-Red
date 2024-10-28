d69a4060066585f92126085fb56787578781812d
Chance B. Loveday <141525952+cloveday098@users.noreply.github.com>
2024-10-28 13:39:27 -0400

    All 53 currency unit tests work; add a separate unit test for Venezulan Bolivar

--- Team_Red/test/currency_calcTest.java
+++ Team_Red/test/currency_calcTest.java
@@ -37,59 +37,59 @@ public class currency_calcTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-            //{"Australian Dollar", "Argentine Peso", "2500", 1234}, //idk
+            {"Australian Dollar", "Argentine Peso", "2500", 1631518},
             {"Australian Dollar", "Australian Dollar", "2500", 2500},
-            //{"Australian Dollar", "Bahraini Dinar", "2500", 675},  //idk
-            //{"Australian Dollar", "Botswana Pula", "2500", 2350},  //idk
+            {"Australian Dollar", "Bahraini Dinar", "2500", 622},
+            {"Australian Dollar", "Botswana Pula", "2500", 22128},
             {"Australian Dollar", "Brazilian Real", "2500", 9424},
             {"Australian Dollar", "British Pound", "2500", 1273},
-            //{"Australian Dollar", "Bruneian Dollar", "2500", 2500},  //idk
-            //{"Australian Dollar", "Bulgarian Lev", "2500", 1534},  //idk
-            //{"Australian Dollar", "Canadian Dollar", "2500", 2293},
-            //{"Australian Dollar", "Chilean Peso", "2500", 1800},  //idk
-            //{"Australian Dollar", "Chinese Yuan Renminbi", "2500", 11755},
-            //{"Australian Dollar", "Colombian Peso", "2500", 920},  //idk
-            //{"Australian Dollar", "Czech Koruna", "2500", 1450},  //idk
-            //{"Australian Dollar", "Danish Krone", "2500", 1350},  //idk
-            //{"Australian Dollar", "Emirati Dirham", "2500", 2435},  //idk
+            {"Australian Dollar", "Bruneian Dollar", "2500", 2186},  
+            {"Australian Dollar", "Bulgarian Lev", "2500", 2986},
+            {"Australian Dollar", "Canadian Dollar", "2500", 2293},
+            {"Australian Dollar", "Chilean Peso", "2500", 1561408},
+            {"Australian Dollar", "Chinese Yuan Renminbi", "2500", 11755},
+            {"Australian Dollar", "Colombian Peso", "2500", 7142776},
+            {"Australian Dollar", "Czech Koruna", "2500", 38683},
+            {"Australian Dollar", "Danish Krone", "2500", 11386},
+            {"Australian Dollar", "Emirati Dirham", "2500", 6067},
             {"Australian Dollar", "Euro", "2500", 1529},
             {"Australian Dollar", "Hong Kong Dollar", "2500", 12829},
-            //{"Australian Dollar", "Hungarian Forint", "2500", 1750},  //idk
-            //{"Australian Dollar", "Icelandic Krona", "2500", 1250},  //idk
+            {"Australian Dollar", "Hungarian Forint", "2500", 618011},
+            {"Australian Dollar", "Icelandic Krona", "2500", 227614},
             {"Australian Dollar", "Indian Rupee", "2500", 138856},
-            //{"Australian Dollar", "Indonesian Rupiah", "2500", 1000},  //idk
-            //{"Australian Dollar", "Iranian Rial", "2500", 750},  //idk
-            //{"Australian Dollar", "Israeli Shekel", "2500", 2300},  //idk
+            {"Australian Dollar", "Indonesian Rupiah", "2500", 25883699},
+            {"Australian Dollar", "Iranian Rial", "2500", 69568213},
+            {"Australian Dollar", "Israeli Shekel", "2500", 6150},
             {"Australian Dollar", "Japanese Yen", "2500", 251438},
-            //{"Australian Dollar", "Kazakhstani Tenge", "2500", 1600},  //idk
-            //{"Australian Dollar", "Kuwaiti Dinar", "2500", 600},  //idk
-            //{"Australian Dollar", "Libyan Dinar", "2500", 1100},  //idk
-            //{"Australian Dollar", "Malaysian Ringgit", "2500", 2000},  //idk
-            //{"Australian Dollar", "Mauritian Rupee", "2500", 1850},  //idk
+            {"Australian Dollar", "Kazakhstani Tenge", "2500", 807508},
+            {"Australian Dollar", "Kuwaiti Dinar", "2500", 505},
+            {"Australian Dollar", "Libyan Dinar", "2500", 7928},
+            {"Australian Dollar", "Malaysian Ringgit", "2500", 7181},
+            {"Australian Dollar", "Mauritian Rupee", "2500", 76122},
             {"Australian Dollar", "Mexican Peso", "2500", 33003},
-            //{"Australian Dollar", "Nepalese Rupee", "2500", 1200},  //idk
-            //{"Australian Dollar", "New Zealand Dollar", "2500", 2500},  //idk
-            //{"Australian Dollar", "Norwegian Krone", "2500", 1400},  //idk
-            //{"Australian Dollar", "Omani Rial", "2500", 700},  //idk
-            //{"Australian Dollar", "Pakistani Rupee", "2500", 1500},  //idk
-            //{"Australian Dollar", "Philippine Peso", "2500", 1300},  //idk
-            //{"Australian Dollar", "Polish Zloty", "2500", 1600},  //idk
-            //{"Australian Dollar", "Qatari Riyal", "2500", 1900},  //idk
-            //{"Australian Dollar", "Romanian New Leu", "2500", 1700},  //idk
+            {"Australian Dollar", "Nepalese Rupee", "2500", 221376},
+            {"Australian Dollar", "New Zealand Dollar", "2500", 2754},
+            {"Australian Dollar", "Norwegian Krone", "2500", 18093},
+            {"Australian Dollar", "Omani Rial", "2500", 634},
+            {"Australian Dollar", "Pakistani Rupee", "2500", 457700},
+            {"Australian Dollar", "Philippine Peso", "2500", 95909},
+            {"Australian Dollar", "Polish Zloty", "2500", 6622},
+            {"Australian Dollar", "Qatari Riyal", "2500", 6000},
+            {"Australian Dollar", "Romanian New Leu", "2500", 7570},
             {"Australian Dollar", "Russian Ruble", "2500", 160132},
-            //{"Australian Dollar", "Saudi Arabian Riyal", "2500", 2400},  //idk
+            {"Australian Dollar", "Saudi Arabian Riyal", "2500", 6184},
             {"Australian Dollar", "Singapore Dollar", "2500", 2181},
             {"Australian Dollar", "South African Rand", "2500", 29184},
             {"Australian Dollar", "South Korean Won", "2500", 2292734},
-            //{"Australian Dollar", "Sri Lankan Rupee", "2500", 1250},  //idk
-            //{"Australian Dollar", "Swedish Krona", "2500", 1350},  //idk
+            {"Australian Dollar", "Sri Lankan Rupee", "2500", 483212},
+            {"Australian Dollar", "Swedish Krona", "2500", 17513},
             {"Australian Dollar", "Swiss Franc", "2500", 1431},
-            //{"Australian Dollar", "Taiwan New Dollar", "2500", 1500},  //idk
-            //{"Australian Dollar", "Thai Baht", "2500", 1900},  //idk
-            //{"Australian Dollar", "Trinidadian Dollar", "2500", 2500},  //idk
-            //{"Australian Dollar", "Turkish Lira", "2500", 1400},  //idk
+            {"Australian Dollar", "Taiwan New Dollar", "2500", 52869},
+            {"Australian Dollar", "Thai Baht", "2500", 55646},
+            {"Australian Dollar", "Trinidadian Dollar", "2500", 11160},
+            {"Australian Dollar", "Turkish Lira", "2500", 56453},
             {"Australian Dollar", "US Dollar", "2500", 1652},
-            //{"Australian Dollar", "Venezuelan Bolivar", "2500", 800}  //idk
+            {"Australian Dollar", "Venezuelan Bolivar", "2500", 2147483647}
         });
     }
     /**
@@ -106,7 +106,7 @@ public class currency_calcTest {
         //int expResult = 1529;
         int result = instance.convert(oldCurr, newCurr, oldAmt).intValue();
         System.out.println(5 + (result * expResult/result));
-        assertEquals(expResult, result, result * expResult/result);
+        assertEquals(expResult, result, result * 0.05);
     }
 
     /**
